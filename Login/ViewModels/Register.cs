00d213dd642d41d1dad8a59ba4524d83f7212c7f
TAMILVANAN2108 <60386930+TAMILVANAN2108@users.noreply.github.com>
2022-03-24 07:46:22 +0530

    Add project files.

---
+++ Login/ViewModels/Register.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login.ViewModels
+{
+    public class Register
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password),ErrorMessage ="Password and Confirm Pasword Not Mached")]
+        public string ConfirmPassword { get; set; }
+    }
+}
